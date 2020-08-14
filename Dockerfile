32c97dcfd900fa258cf7caa3779fa0073cc396ef
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2020-08-14 18:19:10 +0200

    fix when no PUID and PGID provided

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,5 @@ EXPOSE 80
 COPY webdav.conf /etc/nginx/nginx.template.conf
 
 COPY run.sh /
-RUN chmod +x run.sh
+RUN chmod +x run.sh && rm /etc/nginx/nginx.conf
 CMD /run.sh
--- run.sh
+++ run.sh
@@ -1,31 +1,41 @@
 #!/bin/sh
 
-export DAV_METHODS=${DAV_METHODS:=PUT DELETE MKCOL COPY MOVE}
-PUID=${PUID}
-PGID=${PGID}
-#TZ
+if [ ! -f /etc/nginx/nginx.conf ]; then
 
-export USER
-export GROUP
+    echo "Configure WebDAV"
 
-if [ -n $PGID ]; then
-    addgroup -g $PGID group
-    GROUP=group
-fi
+    export DAV_METHODS=${DAV_METHODS:=PUT DELETE MKCOL COPY MOVE}
+    PUID=${PUID}
+    PGID=${PGID}
+    #TZ
 
-if [ -n $PUID ]; then
-    if [ -n $PGID ]; then
-        adduser --disabled-password --uid $PUID --ingroup group user
-    else
-        adduser --disabled-password --uid $PUID user
+    export USER
+    export GROUP
+
+    if [ -n "$PGID" ]; then
+        echo "Creating group"
+        addgroup -g $PGID group
+        GROUP=group
     fi
-    USER=user
-else
-    USER=nginx
+
+    if [ -n "$PUID" ]; then
+        echo "Creating user"
+        if [ -n "$PGID" ]; then
+            adduser --disabled-password --uid $PUID --ingroup group user
+        else
+            adduser --disabled-password --uid $PUID user
+        fi
+        USER=user
+    else
+        USER=nginx
+    fi
+
+    export LOG_FORMAT='$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" "$http_x_forwarded_for"'
+
+    envsubst < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
+
 fi
 
-export LOG_FORMAT='$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" "$http_x_forwarded_for"'
-
-envsubst < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
+echo "Start !"
 
 exec nginx -g "daemon off;"
